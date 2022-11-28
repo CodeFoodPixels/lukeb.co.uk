87526651a4306a2b7f2ddd838039c9728683faab
Luke Bonaccorsi <luke@lukeb.co.uk>
2022-11-28 13:57:14 +0000

    Use https.request correctly

--- netlify/functions/build.js
+++ netlify/functions/build.js
@@ -2,7 +2,7 @@ const { request } = require("node:https");
 const { schedule } = require("@netlify/functions");
 // const { cron } = require("../../dist/nextbuild.json");
 
-exports.handler = schedule("50 13 28 11 1", (event, context) => {
+exports.handler = schedule("00 14 28 11 1", (event, context) => {
   const options = {
     hostname: "api.netlify.com",
     path: `/build_hooks/${process.env.BUILD_HOOK}`,
